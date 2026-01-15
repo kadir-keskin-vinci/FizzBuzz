a6f42defeb150436114fa0ec307aa302ecbc2e27
kadir-keskin-vinci <kadir.keskin@student.vinci.be>
2026-01-15 16:31:44 +0100

    refactor: manage multiple of 3

--- FizzBuzz.ts
+++ FizzBuzz.ts
@@ -1,6 +1,6 @@
 export class FizzBuzz{
     count(input:number):string {
-        if(input === 3 || input === 6)return "fizz";
+        if(input % 3 === 0)return "fizz";
         if(input === 5) return "buzz";
        return `${input}`
     }

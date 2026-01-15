fa1993b2637c74c4d1cc97afcfc117cafbe1fb26
kadir-keskin-vinci <kadir.keskin@student.vinci.be>
2026-01-15 15:52:47 +0100

    refactor: factorise FizzBuzz class instaction in tests

--- FizzBuzz.test.ts
+++ FizzBuzz.test.ts
@@ -1,13 +1,12 @@
 import { FizzBuzz } from "./FizzBuzz";
 
 describe('FizzBuzz', () =>{
-
+let fizzBuzz = new FizzBuzz();
 
     describe('count method', () =>{
         it("should return 1 when given 1", () =>{
 
             //Arrange
-            let fizzBuzz = new FizzBuzz();
 
             //Act
             let actual = fizzBuzz.count(1);
@@ -17,13 +16,13 @@ describe('FizzBuzz', () =>{
         });
 
          it("should return 2 when given 2", () =>{
-            let fizzBuzz = new FizzBuzz();
+            
             let actual = fizzBuzz.count(2);
 
             expect(actual).toBe("2");
         });
         it("should return fizz when given 3", () =>{
-            let fizzBuzz = new FizzBuzz();
+            
             let actual = fizzBuzz.count(3);
             expect(actual).toBe("fizz");
         })

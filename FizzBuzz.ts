261ddc1b1a5b66d88915cc29b485c2804a0ce799
kadir-keskin-vinci <kadir.keskin@student.vinci.be>
2026-01-15 16:51:14 +0100

    refactor: extract funtion  multiple of 3 and 5

--- FizzBuzz.ts
+++ FizzBuzz.ts
@@ -1,11 +1,15 @@
 export class FizzBuzz{
     count(input:number):string {
-        if(this.isMultipleOfThree(input) && this.isMultipleOfFive(input))return "fizzbuzz";
+        if(this.isMultipleOfThreeAndFive(input))return "fizzbuzz";
         if(this.isMultipleOfThree(input))return "fizz";
         if(this.isMultipleOfFive(input)) return "buzz";
        return `${input}`
     }
     
+
+    private isMultipleOfThreeAndFive(input: number) {
+        return this.isMultipleOfThree(input) && this.isMultipleOfFive(input);
+    }
 
     private isMultipleOfFive(input: number):boolean {
         return input % 5 === 0;

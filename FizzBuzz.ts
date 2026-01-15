91baab5b475c15f07fccc08b7dfaa5fce5471f8a
kadir-keskin-vinci <kadir.keskin@student.vinci.be>
2026-01-15 16:41:22 +0100

    refactor: extract multiple of 5

--- FizzBuzz.ts
+++ FizzBuzz.ts
@@ -1,10 +1,14 @@
 export class FizzBuzz{
     count(input:number):string {
         if(this.isMultipleOfThree(input))return "fizz";
-        if(input%5 === 0) return "buzz";
+        if(this.isMultipleOfFive(input)) return "buzz";
        return `${input}`
     }
     
+
+    private isMultipleOfFive(input: number):boolean {
+        return input % 5 === 0;
+    }
 
     private isMultipleOfThree(input: number): boolean {
         return input % 3 === 0;
